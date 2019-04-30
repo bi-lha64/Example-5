72691253a0b6c0eee023964f426acb5e8aadece1
wanjiku <wanjikubilha64@gmail.com>
2019-04-30 19:34:17 +0300

    example 5

---
+++ example 5/shapes.py
@@ -0,0 +1,46 @@
+class Circle():
+	def__init__(self,r):
+	self.r=r
+
+	def area(self):
+		return self.radius**2*3.14
+
+	def circumference(self):
+		return 2*self.radius*3.14
+
+class Square
+	def__init__(self,a):
+	self.area=a
+
+	def area(self):
+		return self.area(a*a)
+
+	def perimeter(self):
+		return self.perimeter(a*a*a*a)
+
+class Rectangle():
+	def__init__(self,l,w):
+	self.length=l
+	self.width=w
+
+	def rectangle_area(self):
+		 return self.length*self.width
+
+class Sphere(self):
+	def_int__(self,r):
+	self.r=r
+
+	def surface(self ):
+        r = self.radius
+        self.area = 4 * 3.14 * (r*r)
+        return (self.area)
+
+	
+	def volume(self):
+		r=self.radius
+		self.volume = (4/3) * 3.14 (r*r*r)
+		return(self.volume)
+
+
+
+
